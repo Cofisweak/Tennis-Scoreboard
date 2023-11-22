c84fe995ad5b1497356f604f03952a6d28e06463
cofisweak <cofisweak@gmail.com>
2023-11-22 13:03:07 +0300

    configure model

--- src/main/java/com/cofisweak/model/Match.java
+++ src/main/java/com/cofisweak/model/Match.java
@@ -11,6 +11,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+@Table(name = "matches")
 public class Match {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
